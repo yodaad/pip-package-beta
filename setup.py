fd3a4b948ed9e7ab032898360e8eb2567a39c032
Diego Arango <yodaad@gmail.com>
2025-09-17 10:53:09 -0500

    Release No.1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pip-package-beta",                              # Package name
+    version="0.1.0",                                      # Initial version
+    packages=find_packages(),                             # Packages to include
+    description="A simple greeting pip package",          # Brief description
+    author="Diego Arango",                                # Name
+    author_email="yodaad@gmail.com",                      # Email
+    url="https://github.com/yodaad/pip-package-beta",     # Project's URL 
+)
